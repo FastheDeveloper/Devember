735ae9dbbe7c77139c3fec5ef880fd2ad2b3138e
FastheDeveloper <seriki2farou@gmail.com>
2023-12-26 11:56:29 +0100

    Indicator

--- src/components/day7/MemoListItem.tsx
+++ src/components/day7/MemoListItem.tsx
@@ -1,15 +1,99 @@
 import { StyleSheet, Text, View } from "react-native"
-import React from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { FontAwesome5 } from "@expo/vector-icons"
+import { AVPlaybackStatus, Audio } from "expo-av"
+import { Sound } from "expo-av/build/Audio"
+import Animated, { useAnimatedStyle, withTiming } from "react-native-reanimated"
 const MemoListItem = ({ uri }: { uri: string }) => {
-  const progress = 20
+  const [sound, setSound] = useState<Sound>()
+  const [status, setStatus] = useState<AVPlaybackStatus>()
+  async function loadSound() {
+    console.log("Loading Sound")
+    const { sound } = await Audio.Sound.createAsync(
+      { uri: uri },
+      { progressUpdateIntervalMillis: 1000 / 60 },
+      //   undefined,
+      onPlaybackStatusUpdate
+    )
+    setSound(sound)
+  }
+
+  const onPlaybackStatusUpdate = useCallback(
+    async (newStatus: AVPlaybackStatus) => {
+      setStatus(newStatus)
+      if (!newStatus.isLoaded || !sound) {
+        return
+      }
+      if (newStatus.didJustFinish) {
+        console.warn("SHould resatart")
+        await sound?.setPositionAsync(0)
+        //   sound?.replayAsync()
+      }
+    },
+    [sound]
+  )
+  useEffect(() => {
+    loadSound()
+  }, [uri])
+
+  async function playSound() {
+    if (!sound) {
+      return
+    }
+
+    if (status?.isLoaded && status.isPlaying) {
+      await sound.pauseAsync()
+    } else if (
+      status?.isLoaded &&
+      status.positionMillis === status.durationMillis
+    ) {
+      await sound.replayAsync()
+    } else {
+      await sound.playAsync()
+    }
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound")
+          sound.unloadAsync()
+        }
+      : undefined
+  }, [sound])
+
+  const isPlaying = status?.isLoaded ? status.isPlaying : false
+  const position = status?.isLoaded ? status.positionMillis : 0
+  const duration = status?.isLoaded ? status.durationMillis : 1
+
+  const progress = duration && position / duration
+
+  const animatedIdicator = useAnimatedStyle(() => ({
+    left: progress && `${progress * 100}%`,
+  }))
+  const formatMillis = (millisecond: number) => {
+    const minutes = Math.floor(millisecond / (1000 * 60))
+    const seconds = Math.floor((millisecond % (1000 * 60)) / 1000)
+
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`
+  }
 
   return (
     <View style={styles.container}>
-      <FontAwesome5 name={"play"} size={20} color={"gray"} />
+      <FontAwesome5
+        onPress={playSound}
+        name={isPlaying ? "pause" : "play"}
+        size={20}
+        color={"gray"}
+      />
       <View style={styles.playBackContainer}>
         <View style={styles.playbackGround} />
-        <View style={[styles.playbackINdicator, { left: `${progress}%` }]} />
+        <Animated.View style={[styles.playbackINdicator, animatedIdicator]} />
+        <Text
+          style={{ position: "absolute", right: 0, bottom: 0, color: "gray" }}
+        >
+          {formatMillis(position || 0)}/{formatMillis(duration || 0)}
+        </Text>
       </View>
     </View>
   )
@@ -23,9 +107,11 @@ const styles = StyleSheet.create({
     margin: 5,
     flexDirection: "row",
     alignItems: "center",
-    padding: 15,
+    paddingHorizontal: 15,
     borderRadius: 10,
     gap: 15,
+    paddingVertical: 5,
+
     //Shadow
     shadowColor: "#000",
     shadowOffset: {
@@ -39,7 +125,7 @@ const styles = StyleSheet.create({
   },
   playBackContainer: {
     flex: 1,
-    height: 30,
+    height: 50,
     justifyContent: "center",
   },
   playbackGround: {
@@ -48,7 +134,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   playbackINdicator: {
-    width: 15,
+    width: 10,
     aspectRatio: 1,
     backgroundColor: "royalblue",
     borderRadius: 10,
